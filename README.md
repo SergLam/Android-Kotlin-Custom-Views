53ff72ff6c6237d55e0d15bc879e7d980c6446a6
Serhii Liamtsev <sliamtsev@griddynamics.com>
2021-07-14 12:35:25 +0300

    Finished tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+Training project for android custom views drawing:
+
+https://developer.android.com/codelabs/advanced-android-kotlin-training-custom-views#0
--- app/src/main/java/com/serglam/androidcustomviews/MainActivity.kt
+++ app/src/main/java/com/serglam/androidcustomviews/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.serglam.androidcustomviews
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dialView: DialView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dialView = findViewById(R.id.dialView)
     }
 }
